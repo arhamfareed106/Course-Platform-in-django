bcb11d5c36d83bc00843816d3d629bc40f8d4152
arhamfareed106 <arhamfareed575@gmail.com>
2024-10-24 20:25:45 +0500

    new

--- course/views.py
+++ course/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+
+import course
+from course.models import Course
 
 # Create your views here.
 
 @login_required
 def course_list(request, ):
-    return render (request, "base.html")
+    courses= Course.objects.all()
+
+    if request.user.is_authenticated:
+        for course in course:
+            course.is_valid = request.user in course.subscribers.all()
+    else:
+        for course in course:
+            course.is_valid = False
+
+    context ={
+        "courses": courses
+    }
+    return render (request, "course_list.html" , context)
 
 @login_required
-def course_detail(request, ):
-    return render (request, "base.html")
+def course_detail(request, course_id ):
+    course = get_object_or_404(Course, id =course_id)
+
+    if request.user not in course.subscribers.all():
+        return redirect("course_list")
+    context={
+        "course": course
+    }
+    # return render (request, "course_detail.html" context)
--- user/urls.py
+++ user/urls.py
@@ -13,3 +13,6 @@ urlpatterns = [
     path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
     path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done")
 ]
+
+
+
